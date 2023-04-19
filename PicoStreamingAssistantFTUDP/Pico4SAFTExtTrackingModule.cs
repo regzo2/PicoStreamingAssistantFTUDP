dae7b6af29f3e07549659f00a6e1179b0c4c2693
regzo2 <regzo252@gmail.com>
2023-04-19 07:18:23 -0400

    Refactored into a VRCFaceTracking ExtTrackingModule module

---
+++ PicoStreamingAssistantFTUDP/Pico4SAFTExtTrackingModule.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using VRCFaceTracking;
+using static Pxr;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Pico4SAFTExtTrackingModule : ExtTrackingModule
+{
+    const string IP_ADDRESS = "127.0.0.1";
+    const int PORT_NUMBER = 29763; // Temporary port as of current Pico 4 SA app.
+
+    private static UdpClient udpClient = new UdpClient(PORT_NUMBER);
+    private static IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(IP_ADDRESS), PORT_NUMBER);
+    private static byte[] receiveBytes = new byte[2048];
+    private static PxrFTInfo data = new();
+
+    public override (bool SupportsEye, bool SupportsExpression) Supported => (true, true);
+
+    public override (bool eyeSuccess, bool expressionSuccess) Initialize(bool eyeAvailable, bool expressionAvailable)
+    {
+        udpClient = new UdpClient(PORT_NUMBER);
+        endPoint = new IPEndPoint(IPAddress.Parse(IP_ADDRESS), PORT_NUMBER);
+
+        return RetrieveStreamData(ref receiveBytes);
+    }
+
+    public static (bool, bool) RetrieveStreamData( ref byte[] bytes)
+    {
+        try
+        {
+            receiveBytes = udpClient.Receive(ref endPoint);
+
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PxrFTInfo)));
+            Marshal.Copy(receiveBytes, 0, ptr, Marshal.SizeOf(typeof(PxrFTInfo)));
+            data = (PxrFTInfo)Marshal.PtrToStructure(ptr, typeof(PxrFTInfo));
+
+            Marshal.FreeHGlobal(ptr);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        return (true, true);
+    }
+
+    public override void Update()
+    {
+        RetrieveStreamData(ref receiveBytes);
+
+        Logger.LogInformation("JawOpen: ", data.blendShapeWeight[(int)BlendShapeIndex.JawOpen].ToString());
+    }
+
+    public override void Teardown()
+    {
+        udpClient.Dispose();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Pico Streaming Assistant Face Tracking UDP
+# VRCFaceTracking - Pico UDP
 
 Example of how the Pico 4 Pro sends face tracking data 
-using the Pico Streaming Assistant via UDP.
+using the Pico Streaming Assistant via UDP in a VRCFaceTracking 
+module.
