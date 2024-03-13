d9fa3176eea9ec9947df949f9d01259e044c96e0
rogermiranda1000 <rogermirandaperez@hotmail.com>
2024-03-13 18:10:10 +0100

    Feature: instantiate legacy connector depending on PICO Connect config 1/2

--- PicoStreamingAssistantFTUDP/PicoConnectors/ConfigChecker/PicoConnect/PicoConnectConfigChecker.cs
+++ PicoStreamingAssistantFTUDP/PicoConnectors/ConfigChecker/PicoConnect/PicoConnectConfigChecker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace Pico4SAFTExtTrackingModule.PicoConnectors.ConfigChecker.PicoConnect;
 
 public sealed class PicoConnectConfigChecker : IConfigChecker
 {
+    readonly IFileSystem fileSystem;
+
+    public PicoConnectConfigChecker(IFileSystem fileSystem)
+    {
+        this.fileSystem = fileSystem;
+    }
+
+    public PicoConnectConfigChecker() : this(new FileSystem()) { }
+
     public int GetTransferProtocolNumber(PicoPrograms program)
     {
         if (program != PicoPrograms.PicoConnect) throw new ArgumentException("PicoConnectConfigChecker class only checks for PICO Connect config files");
