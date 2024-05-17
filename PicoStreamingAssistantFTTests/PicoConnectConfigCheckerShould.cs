b523b803ad4252aceca3864fb2061584b8ac91b2
SomezakiKureha <lonelyicer@qq.com>
2024-05-18 05:18:40 +0800

    UnitTest: update new config file

--- PicoStreamingAssistantFTTests/PicoConnectConfigCheckerShould.cs
+++ PicoStreamingAssistantFTTests/PicoConnectConfigCheckerShould.cs
@@ -35,9 +35,56 @@ public class PicoConnectConfigCheckerShould
         return logger;
     }
 
-    public static string GetSettingsJson(int transferProtocol = 0)
+    public static string GetLegacySettingsJson(int transferProtocol = 0)
     {
         return "{\"video\":{\"resolution\":\"ultra\",\"bitrate\":{\"smooth\":20,\"sd\":60,\"hd\":100,\"ultra\":150},\"autoBitrate\":false,\"refreshRate90Hz\":true,\"frameBuffer\":true,\"codec\":\"hevc\",\"asw\":false,\"sharpenRate\":75},\"audio\":{\"mic\":false,\"volume\":50,\"output\":\"both\",\"latency\":30},\"general\":{\"autoConnect\":\"off\",\"language\":\"es-ES\"},\"lab\":{\"quic\":false,\"superResolution\":true,\"gamma\":100,\"faceTrackingMode\":4,\"faceTrackingTransferProtocol\":" + transferProtocol + ",\"bodyTracking\":false,\"controllerSensitivity\":50}}";
+    }
+
+    public static string GetSettingsJson(int transferProtocol = 0)
+    {
+        return "{\"video\":{\"resolution\":\"hd\",\"bitrate\":{\"smooth\":20,\"sd\":150,\"hd\":100,\"ultra\":150},\"autoBitrate\":false,\"refreshRate90Hz\":true,\"frameBuffer\":true,\"codec\":\"hevc\",\"asw\":false,\"sharpenRate\":75},\"audio\":{\"mic\":true,\"volume\":58,\"output\":\"both\",\"latency\":30},\"general\":{\"autoConnect\":false,\"language\":\"zh-Hans-CN\",\"loginItem\":true,\"closeToTray\":false},\"lab\":{\"quic\":false,\"superResolution\":false,\"gamma\":100,\"faceTrackingMode\":4,\"faceTrackingTransferProtocol\":" + transferProtocol + ",\"bodyTracking\":false,\"controllerSensitivity\":49},\"game\":{\"resolution\":\"hd\",\"bitrate\":{\"smooth\":20,\"sd\":150,\"hd\":100,\"ultra\":150,\"uhd\":150},\"autoBitrate\":false,\"refreshRate90Hz\":true,\"frameBuffer\":false,\"codec\":\"avc\",\"asw\":false,\"sharpenRate\":75,\"superResolution\":true,\"gamma\":1,\"refreshRate\":90},\"desktop\":{\"sharpenRate\":0}}";
+    }
+
+    [TestMethod]
+    public void ReturnTransferProtocol0WhenLegacyFileContainsTransferProtocol0()
+    {
+        // arrange
+        List<string> errors = new List<string>();
+        Mock<ILogger> logger = GetLoggerMock(errors);
+
+        MockFileSystem mockFileSysteme = new MockFileSystem();
+        MockFileData mockFileData = new MockFileData(GetLegacySettingsJson(0));
+        mockFileSysteme.AddFile(CONFIG_FILE_PATH, mockFileData);
+
+        PicoConnectConfigChecker uut = new PicoConnectConfigChecker(logger.Object, mockFileSysteme);
+
+        // act
+        int got = uut.GetTransferProtocolNumber(PicoPrograms.PicoConnect);
+
+        // assert
+        Assert.AreEqual(0, errors.Count, "Expected no errors; instead got:\n" + String.Join("\n", errors));
+        Assert.AreEqual(0, got);
+    }
+
+    [TestMethod]
+    public void ReturnTransferProtocol2WhenLegacyFileContainsTransferProtocol2()
+    {
+        // arrange
+        List<string> errors = new List<string>();
+        Mock<ILogger> logger = GetLoggerMock(errors);
+
+        MockFileSystem mockFileSysteme = new MockFileSystem();
+        MockFileData mockFileData = new MockFileData(GetLegacySettingsJson(2));
+        mockFileSysteme.AddFile(CONFIG_FILE_PATH, mockFileData);
+
+        PicoConnectConfigChecker uut = new PicoConnectConfigChecker(logger.Object, mockFileSysteme);
+
+        // act
+        int got = uut.GetTransferProtocolNumber(PicoPrograms.PicoConnect);
+
+        // assert
+        Assert.AreEqual(0, errors.Count, "Expected no errors; instead got:\n" + String.Join("\n", errors));
+        Assert.AreEqual(2, got);
     }
 
     [TestMethod]
